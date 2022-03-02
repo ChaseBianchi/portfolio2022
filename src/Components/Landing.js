8370b3c831639ee45860cffa5a52e18afabd2a72
ChaseBianchi <73509539+ChaseBianchi@users.noreply.github.com>
2022-03-02 12:47:53 -0500

    styling links

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -18,8 +18,16 @@ function Landing() {
     <div id='landing' className="comp-wrapper">
       <div className="landingBar">
         <div className='emailWrapper navHover'><a className='landingEmail' href="mailto:yourmail@gmail.com" rel='noreferrer' target='_blank'>cbianchi@protonmail.com</a></div>
-        <div className="navHover"><a className='linkedinWrapper' href="https://www.linkedin.com/in/chasebianchi/" target='_blank' rel='noreferrer'><img className='linkedinLogo' src="/media/linkedin3.png" alt="linkedin" /></a></div>
-        <div className="navHover"><a className='linkedinWrapper' href="https://github.com/ChaseBianchi/" target='_blank' rel='noreferrer'><img className='linkedinLogo' src="/media/github2.png" alt="github" /></a></div>
+        <div className="navHover">
+          <a className='linkedinWrapper' href="https://www.linkedin.com/in/chasebianchi/" target='_blank' rel='noreferrer'>
+            <img className='linkedinLogo' src="/media/linkedin3.png" alt="linkedin" />
+          </a>
+        </div>
+        <div className="navHover">
+          <a className='linkedinWrapper' href="https://github.com/ChaseBianchi/" target='_blank' rel='noreferrer'>
+            <img className='linkedinLogo' src="/media/github2.png" alt="github" />
+          </a>
+        </div>
 
       </div>
       <div style={nameStyle} className="textwrapper">
