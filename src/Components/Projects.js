e6c8c7152011f95d603710810cb9d231710913b3
ChaseBianchi <73509539+ChaseBianchi@users.noreply.github.com>
2022-03-08 13:59:15 -0500

    bug fix projects alignment
    animation was being cut off on left side. Removed margin on ProjectContainer and accounted for it in absolute position of each ProjectTile

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -93,7 +93,7 @@ function Projects() {
         <h2>Projects</h2>            
         <div onClick={handleLeftArrow} style={slideIndex===0 ? {opacity: '50%'} : {opacity: '100%'}}className="arrowWrapper leftArrowWrapper"><i className="arrow left"></i></div>
         <div style={{width: `${350*projectArr.length}px`}} className="projectContainer">
-                {projectArr.map(proj=><ProjectTile  key={proj.id} data={proj} leftPx={`${350*(proj.id-slideIndex)}px`}/>)}
+                {projectArr.map(proj=><ProjectTile  key={proj.id} data={proj} leftPx={`${35+350*(proj.id-slideIndex)}px`}/>)}
         </div>
         <div onClick={handleRightArrow} style={slideIndex===projectArr.length-1 ? {opacity: '50%'} : {opacity: '100%'}} className="arrowWrapper rightArrowWrapper"><i className="arrow right"></i></div>
     </div>
