061361ac47b5cd65cee861cbd8fed7ab4c9e7e74
ChaseBianchi <73509539+ChaseBianchi@users.noreply.github.com>
2022-03-08 12:31:31 -0500

    cleanup unused files

--- src/App.js
+++ src/App.js
@@ -4,19 +4,13 @@ import Projects from './Components/Projects';
 import Experience from './Components/Experience';
 import About from './Components/About';
 import Footer from './Components/Footer';
-// import Nav from './Components/Nav';
 import Connect from './Components/Connect';
 import './App.scss';
-// import NavMobile from './Components/NavMobile';
-// import NavDesktop from './Components/NavDesktop';
 
 function App() {
-  // const [isOpen, setIsOpen] = useState(false)
-  // const handleMenuClose = () => setIsOpen(false)
-  // const handleMenuOpen = () => setIsOpen(true)
+
   return (
     <div className="App">
-      {/* <Nav isOpen={isOpen} handleMenuClose={handleMenuClose} handleMenuOpen={handleMenuOpen}/> */}
       <Landing />
       <Projects/>
       <Experience />
